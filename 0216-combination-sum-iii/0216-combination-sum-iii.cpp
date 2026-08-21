dc376602d6721e8b807155aff1eddee5743a0049
krishna-collab12 <krishnaa15g@gmail.com>
2026-08-21 17:24:16 +0530

    Time: 6 ms (5.06%), Space: 14.3 MB (7.04%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    void combo(vector<vector<int>> &ans ,vector<int> arr ,int sum ,int i , int n ,int target , int k ){
-        if(sum == target && k == n ){
+    void combo(vector<vector<int>> &ans ,vector<int> arr ,int sum ,int num , int count ,int target , int k ){
+        if(sum == target && k == count ){
             ans.push_back(arr);
             return ; 
         }
-        else if( sum + i > target ){
+        else if( sum + num > target ){
             return ; 
         }
-        if(i == 10 || k == n ){
+        if(num == 10 || k == count ){
             return ; 
         }
-        arr.push_back(i);
-        combo(ans , arr , sum + i , i+1 , n+1 , target , k );
+        arr.push_back(num);
+        combo(ans , arr , sum + num , num+1 , count+1 , target , k );
         arr.pop_back();
-        combo(ans , arr , sum , i+1 , n , target , k );
+        combo(ans , arr , sum , num+1 , count , target , k );
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        int low = k*(k+1) / 2 ;
-        int high = (9-k)*k + low ; 
+        // int low = k*(k+1) / 2 ;
+        // int high = (9-k)*k + low ; 
+        // if(n < low || n > high ){
+        //     return ans; 
+        // }
         vector<vector<int>> ans ; 
         vector<int> arr ; 
-        if(n < low || n > high ){
-            return ans; 
-        }
         combo(ans,arr,0,1,0,n,k);
         return ans ; 
     }
