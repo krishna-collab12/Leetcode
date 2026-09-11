d6dcb7ce244e70337d3017a4bd5699a67224ea40
krishna-collab12 <krishnaa15g@gmail.com>
2026-09-11 17:09:36 +0530

    Time: 3 ms (26.73%), Space: 10 MB (59.66%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,7 +3,7 @@ public:
     vector<vector<int>> subsets(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> ans ;
-        for(int i = 0 ; i < pow(2,n) ; i++){
+        for(int i = 0 ; i < (1<<n) ; i++){
             vector<int> temp; 
             int x = 0 , a = i ;
             while(a){
